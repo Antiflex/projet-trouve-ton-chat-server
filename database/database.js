e7a36829ba0d6edd704b7009e56609a1c2559935
Antiflex <68072873+Antiflex@users.noreply.github.com>
2024-05-05 21:52:59 +0200

    fix exports + node version

--- database/database.js
+++ database/database.js
@@ -2,10 +2,8 @@ import knex from 'knex';
 
 const connection = process.env.PORT || 'postgres://trouve_ton_chat_dphi_user:GMIvhkvzS3vLZlKredzGgNASkams71cq@dpg-cons6ti1hbls73fprc00-a.virginia-postgres.render.com/trouve_ton_chat_dphi?ssl=true';
 
-const configuredKnex = knex({
+export const db = knex({
     client: 'pg',
     connection: connection,
     searchPath: ['knex', 'public'],
 });
-
-export default configuredKnex;
