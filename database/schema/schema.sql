e9046b9c19598437a2ccff3c7f0a8f6a8846b42b
ALexandre <68072873+Antiflex@users.noreply.github.com>
2024-05-06 08:10:09 +0200

    update schema.sql

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -1,17 +1,22 @@
+DROP TABLE IF EXISTS Chat;
+DROP TABLE IF EXISTS Annonce;
+
 CREATE TABLE Chat(
-   id_chat INT,
-   race VARCHAR(50),
-   couleur_fourrure VARCHAR(50),
-   couleur_yeux VARCHAR(50),
-   PRIMARY KEY(id_chat)
+   	id_chat INT,
+   	race VARCHAR(50),
+   	couleur_fourrure VARCHAR(50),
+   	couleur_yeux VARCHAR(50),
+	PRIMARY KEY(id_chat)
 );
 
 CREATE TABLE Annonce(
-   id_annonce INT,
-   date_annonce DATETIME,
-   annotations VARCHAR(50),
-   id_chat INT NOT NULL,
-   PRIMARY KEY(id_annonce),
-   UNIQUE(id_chat),
-   FOREIGN KEY(id_chat) REFERENCES Chat(id_chat)
+   	id_annonce INT,
+	type_annonce VARCHAR,
+	emplacement GEOGRAPHY,
+   	date_annonce DATETIME,
+   	annotations VARCHAR(50),
+   	id_chat INT NOT NULL,
+   	PRIMARY KEY(id_annonce),
+   	UNIQUE(id_chat),
+   	FOREIGN KEY(id_chat) REFERENCES Chat(id_chat)
 );
