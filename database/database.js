9bdab658951b8d6b43b5c81e771f2fe99f831418
ALexandre <68072873+Antiflex@users.noreply.github.com>
2024-05-06 10:25:30 +0200

    fix env conncetion string

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 import knex from 'knex';
 
-const connection = process.env.PORT || 'postgres://trouve_ton_chat_dphi_user:GMIvhkvzS3vLZlKredzGgNASkams71cq@dpg-cons6ti1hbls73fprc00-a.virginia-postgres.render.com/trouve_ton_chat_dphi?ssl=true';
+const connection = process.env.PG_CONNECTION_STRING || 'postgres://trouve_ton_chat_dphi_user:GMIvhkvzS3vLZlKredzGgNASkams71cq@dpg-cons6ti1hbls73fprc00-a.virginia-postgres.render.com/trouve_ton_chat_dphi?ssl=true';
 
 export const db = knex({
     client: 'pg',
